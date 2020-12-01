88e26911105157526678c1b45201a29a579bc5bb
dwisecar <dwisecar@gmail.com>
2020-11-30 17:51:45 -0800

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+    def kind
+      nonzero_check
+      inequality_check
+      if a == b && b == c
+        :equilateral
+      elsif a == b || a == c || b == c
+        :isosceles
+      else
+        :scalene
+      end
+
+    end
+
+    def nonzero_check
+      if a <= 0 || b <= 0 || c <= 0
+        raise TriangleError
+      end
+    end
+
+    def inequality_check
+      if (a+b<=c) || (a+c<=b) || (b+c<=a)
+        raise TriangleError
+      end
+    end
+
+    class TriangleError < StandardError
+    end
 end
